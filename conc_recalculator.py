09a3449a06222805a1e2b34f3729722dbb30e02e
Isak Sylvin <i.sylvin@null.net>
2016-01-18 06:43:31 +0100

    pc switch, first db calls

--- conc_recalculator.py
+++ conc_recalculator.py
@@ -1,48 +1,39 @@
-import re
-from collections import defaultdict, OrderedDict, Counter
-import time
 import pdb
-import sys
-
-import socket
-import os
+import json
 import couchdb
-import glob
-import re
-import logging
-
-sys.setrecursionlimit(10000) # Overrides recursion limit
+from collections import defaultdict
+import unicodedata
 
 def connection():
     couch = couchdb.Server('http://isak:Purpleplant89@tools-dev.scilifelab.se:5984')
     db = couch['x_flowcells']
     view = db.view('names/project_ids_list')
-    pdb.set_trace()
 
-""" Initializes data from file
-    Caution: File composition is hardcoded
-"""
-def readdata(file):
-    initialBin = defaultdict(set)
-    pattern=re.compile("(P[0-9]{3,5}_[0-9]{3,5}) ([0-9]+) ([0-9]+\.[0-9]+) ([A-Z]+)") 
-
-    for line in file:
-        if pattern.search(line):
-            sample = pattern.search(line).group(1) 
-            remain = (minClust - int(pattern.search(line).group(2)) )
-            lanePerc =  float(pattern.search(line).group(3)) 
-            index =  pattern.search(line).group(4)
-            if (remain > 0): 
-                initialBin[sample] = [remain, index]
-    #pdb.set_trace()
-    mini = 0
-    for k,v in initialBin.items():
-        if v[0] > mini:
-            mini = v[0]
-    print mini
+    fc_track = defaultdict(set)
     
-    return initialBin
+    #Creates a proj -> FCs structure
+    for rec in view.rows:
+        fc = ''.join(rec.key)
+        fc = unicodedata.normalize('NFKD', fc).encode('ascii','ignore')
+        proj = ''.join(rec.value)
+        proj = unicodedata.normalize('NFKD', proj).encode('ascii','ignore')
+        id = ''.join(rec.id)
+        id = unicodedata.normalize('NFKD', id).encode('ascii','ignore')
+        if fc_track[proj] == set([]):
+            fc_track[proj] = dict()
+        fc_track[proj][fc] = id
+    
+    #Creates FC -> sample_info structure
+    map_fun = """function(doc) {
+      emit(doc['name'], doc['illumina']);
+    }"""
+    
+    for red in (db.query(map_fun)).rows:
+        pdb.set_trace()
+        
+    pdb.set_trace()
 
 target_clusters = 320*1000000
 clusters_per_lane = 380*1000000
-connection()
+
+view = connection()
